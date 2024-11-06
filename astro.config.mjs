d2349e5d3dcb6c282dec0d3d47cfed050b6c564a
JuryAndrea <jury.donofrio@gmail.com>
2024-11-06 22:58:56 +0100

    added docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 export default defineConfig({
   // base: '.', // Set a path prefix.
+  outDir: 'docs', // Set the directory to build your site to.
   site: 'https://example.com/', // Use to generate your sitemap and canonical URLs in your final build.
   trailingSlash: 'always', // Use to always append '/' at end of url
   markdown: {
